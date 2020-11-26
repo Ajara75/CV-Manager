aede0253d947aa9be70809ada5c67ac0c1b4d491
Ajara75 <ajararyspekova22@gmail.com>
2020-11-26 12:13:53 +0600

    react

--- react-pizza/src/components/filter/index.js
+++ react-pizza/src/components/filter/index.js
@@ -2,23 +2,20 @@ import React, { useState } from 'react';
 import { filterNames } from './const.js';
 
 function Filter() {
-  // const input = '';
-  // const setInput = input.innerHTML = 'Hatam';
-  const [input, setInput] = useState('');
-  const [names, setNames] = useState(filterNames);
+  const [names] = useState(filterNames);
   const [elIndex, setElIndex] = useState(0);
 
-  const handleChange = (event) => {
-    setInput(event.target.value);
-  };
+  // const handleChange = (event) => {
+  //   setInput(event.target.value);
+  // };
 
-  const handleEnter = (event) => {
-    setInput(event.target.value);
-    if (event.key == 'Enter') {
-      setNames([...names, event.target.value]);
-      setInput('');
-    }
-  };
+  // const handleEnter = (event) => {
+  //   setInput(event.target.value);
+  //   if (event.key === 'Enter') {
+  //     setNames([...names, event.target.value]);
+  //     setInput('');
+  //   }
+  // };
 
   const handleClick = (index) => {
     setElIndex(index);
@@ -26,17 +23,18 @@ function Filter() {
 
   return (
     <div className='filter'>
-      <input
+      {/* <input
         onKeyPress={handleEnter}
         type='text'
         placeholder='Добавь в фильтр новый элемент'
         value={input}
         onChange={handleChange}
-      />
+      /> */}
       <ul className='filter__list'>
         {names.map((item, index) => {
           return (
             <li
+              key={item}
               onClick={() => handleClick(index)}
               className={`filter__item ${index === elIndex ? 'is-active' : ''}`}>
               {item}
