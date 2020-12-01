4354085242e80d94f81a8b88c787cbc0954ee1fc
Ajara75 <ajararyspekova22@gmail.com>
2020-12-01 12:59:26 +0600

    react

--- react-pizza/src/components/menu/index.js
+++ react-pizza/src/components/menu/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Filter from '../filter';
 import Sort from '../sortPopUp';
 import Pizzas from '../pizzas';
 
 function Menu() {
+  const [pizzas, setPizzas] = useState([]);
+  let aiza = [];
+  useEffect(async () => {
+    return await fetch('http://localhost:3000/database.json')
+      .then((response) => response.json())
+      .then(({ pizzas }) => pizzas)
+      .then((array) => setPizzas(array));
+  }, []);
+
+  console.log(pizzas);
+
   return (
     <section className='menu'>
       <div className='container'>
@@ -11,7 +22,15 @@ function Menu() {
           <Filter />
           <Sort />
         </div>
-        <Pizzas />
+        <div className='pizzas'>
+          <h2 className='pizzas__title'>Все пиццы</h2>
+          <div className='row pizzas__row pt-35 ajara'>
+            {pizzas &&
+              pizzas.map((item) => {
+                return <Pizzas {...item} key={item.id} />;
+              })}
+          </div>
+        </div>
       </div>
     </section>
   );
